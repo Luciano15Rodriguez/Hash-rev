a3801190399b402ad3ea7c71c45cbbb48b1ba278
Luciano15Rodriguez <130837064+Luciano15Rodriguez@users.noreply.github.com>
2023-09-22 13:06:45 -0300

    Update revHash.py

--- revHash.py
+++ revHash.py
@@ -2,7 +2,7 @@ import hashlib
 # Modify The Hash in the next line
 hash = "a2099f4c2c2de141afb474dfe4b765ce83448100e77f4359314d94807b00862d53316c03963fc60cbdbd7bc6915778f1830f0f4fd9364a4bc71a09c5e83a0a67"
 
-with open('C:/Users/lumac/Desktop/pass.txt') as file:
+with open('#Pass.txt access route#') as file:
     lines = file.readlines()
     for passwords in lines:
         given_Pass = passwords.strip()
